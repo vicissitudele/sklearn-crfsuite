f8a4c7f626a5740740b4f111aac5f1dd3036a61b
dinh <dinh@dinhs-Mac-Pro.local>
2022-08-14 22:09:29 -0400

    update crf util based on pull request suggestions to match current sklearn interface

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sklearn-crfsuite',
-    version='0.3.6',
+    version='0.3.7-dinh',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
     license='MIT license',
